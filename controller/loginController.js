141d7a7633d04e245759f160b44af3008d5985ef
Tobias Pfaffenzeller <tobler@gmx.com>
2022-01-05 11:13:25 +0100

    LoginUser & creating Token

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,19 +1,36 @@
-const {HttpError }= require("../errors/errorHandler");
-const User = require('../schemas/userSchema')
+const { HttpError } = require("../errors/errorHandler");
+const User = require("../schemas/userSchema");
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 
 module.exports = loginController = {
-   loginUser: (req, res, next) => {
-      console.log("post on /login");
+   loginUser: async (req, res, next) => {
+      try {
+         console.log("post on /login");
+         const userDoesExist = await User.findOne({email: req.body.email});
+         if (!userDoesExist) 
+            return next(new HttpError(500, 'user does not exist'));
+         const token = jwt.sign({email: userDoesExist.email}, process.env.JWTKEY);
+         res.send({message: 'user found and token sent', token})
+      } catch (error) {
+         next(error);
+      }
    },
    logoutUser: (req, res, next) => {
       console.log("post on /logout");
    },
    signupUser: async (req, res, next) => {
-      console.log("post on /signup");
-      await User.create(req.body, (error, userDocument)=>{
-         if (error) 
-         return next(new HttpError(500, 'user not created'))
-         res.send({message: 'new user created'})
-      })
+      try {
+         console.log("post on /signup");
+         await User.create(req.body, (error, userDocument) => {
+            if (error) return next(new HttpError(500, "user not created"));
+            res.send({ message: "new user created" });
+         });
+      } catch (error) {
+         next(error);
+      }
    },
 };
+
+
+//nodemailer
