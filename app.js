fef86fc2cbb51e12f1d1363a4e6fb51f7ca78071
FloHGer <mail@florianhoehle.de>
2022-01-07 14:40:30 +0100

    conflicts solved

--- app.js
+++ app.js
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-// Imports
-const mongoose = require ('mongoose');
-const express = require('express')
-require('dotenv').config()
-const loginController = require ('./controller/loginController')
-const userRouter = require("./routes/userRoute");
-const {routeError, errorHandler} = require ('./errors/errorHandler')
-
-// console.log(errorHandler);
-// Server Start
-const app = express();
-
-//Database Connection
-mongoose.connect(process.env.DB_CONNECT)
-mongoose.connection
- .on('error', console.error)
- .on('connecting', ()=>{
-     console.log('Connecting to Database...')
- })
- .on('connected', ()=>{
-     console.log('Database Connection established!');
- })
-
-
-
-// Middleware
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-
-// Routes
-// app.use((req, res) => res.send('<h1>TRIVIA GAME backend</h1>'));
-
-app.post('/login', loginController.loginUser)
-app.get('/login/:tid', loginController.verifyToken)
-app.post("/logout", loginController.logoutUser);
-app.post("/signup", loginController.signupUser);
-
-
-
-app.use('/user/:uid', userRouter)
-
-
-// Error Handling
-app.use(routeError);
-// if(res.headerSent) return next(error)
-app.use(errorHandler)
-
-=======
 const express = require('express');
 const session = require('express-session');
 const mongoose = require('mongoose');
@@ -95,7 +46,6 @@ app.use('/user/:uid', userRouter);
 app.use(errorController.routeError);
 // if(res.headerSent) return next(error)
 app.use(errorController.errorHandler);
->>>>>>> floh
 
 // Server End
 const port = process.env.PORT || 3003;
