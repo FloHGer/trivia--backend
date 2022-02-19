7ce1ff4a6c7541d9d4b402a54cc96025157e7868
FloHGer <mail@florianhoehle.de>
2022-02-19 17:26:25 +0100

    Change session cookie

--- app.js
+++ app.js
@@ -52,12 +52,12 @@ app.use(express.json());
 app.use(session({
   secret: process.env.SESSION_SECRET,
   cookie: {
-    domain: `.${process.env.FRONTEND}`,
+    domain: 'trivia-ga.me',
     sameSite: none,
     maxAge: 1000 * 60 * 60 * 24 * 30 // 30days
   },
   store,
-  resave: true,
+  resave: false,
   saveUninitialized: false,
 }));
 app.use(passport.initialize());
