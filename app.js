025ba7fcc70221657e4f691b1b2c8c5b6c9913e2
FloHGer <mail@florianhoehle.de>
2022-02-22 11:15:51 +0100

    Cookie settings

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   cookie: {
     maxAge: (1000 * 60 * 60 * 24) * 30, // 30days
-    sameSite: none,
+    sameSite: 'none',
     secure: true,
   },
   store,
