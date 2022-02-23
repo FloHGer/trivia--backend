2e8bf21f580bdd56d6df7f61984a4e4f5107dcd0
FloHGer <mail@florianhoehle.de>
2022-02-23 11:18:40 +0100

    Change email to case insensitive

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -4,7 +4,14 @@ const {Schema, model} = require('mongoose');
 
 const userSchema = new Schema({
   username: {type: String, unique: true, required: true},
-  email: {type: String, unique: true, required: true},
+  email: {
+    type: String,
+    required: true,
+    index: {
+      unique: true,
+      collation: {locale: 'en', strength: 2},
+    }
+  },
   googleId: {type: Number},
   githubId: {type: Number},
   dob: {type: Date, default: null},
